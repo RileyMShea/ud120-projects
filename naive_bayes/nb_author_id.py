3ee747185595d695955f153add8003f57970891e
Riley Shea <RileyMShea@gmail.com>
2019-05-21 07:11:34 -0400

    Made chapter additions to finish quizzes 'IE mini-project'

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
-
+from sklearn.naive_bayes import GaussianNB
+from tools.email_preprocess import preprocess
+from sklearn.metrics import accuracy_score
 """ 
     This is the code to accompany the Lesson 1 (Naive Bayes) mini-project. 
 
@@ -9,25 +11,34 @@
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
-sys.path.append("../tools/")
-from email_preprocess import preprocess
 
+# sys.path.append("../tools/")
 
-### features_train and features_test are the features for the training
-### and testing datasets, respectively
-### labels_train and labels_test are the corresponding item labels
+# features_train and features_test are the features for the training
+# and testing dataset, respectively
+# labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+#########################################################
+# your code goes here ###
 
 
+clf = GaussianNB()
+
+t0 = time()
+# Train the classifier
+clf.fit(features_train, labels_train)
+print("Training time:", round(time()-t0, 3), "s")
+
+t1 = time()
+# Make predictions with our classifier
+pred = clf.predict(features_test)
+print("Prediction time:", round(time()-t1, 3), "s")
+# How accurate are our predictions
+accuracy = accuracy_score(pred, labels_test)
+print(f"Accuracy: {accuracy*100:.2f}%")
 
 #########################################################
-### your code goes here ###
-
-
-#########################################################
-
-
